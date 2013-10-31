fed3fb9a2f82bf02e1ef9753cf04aa83b630fca7
Timothy Andrew <mail@timothyandrew.net>
2013-10-31 14:18:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NilensoRatingApp::Application.config.secret_key_base = '845f936eeb078dbe7e1caade57b52703ecf3abe8fe38c0b90a09e8be8abed314d340225a4cb9a1e7dee3f867f800489d7d3a19bb5750c377a9caa4dd540b05b5'
